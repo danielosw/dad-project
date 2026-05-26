dff9630d79948ee93acfc4cdfb4075708e33a264
danielosw <65432065+danielosw@users.noreply.github.com>
2026-05-26 12:00:27 -0500

    work

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
-
+	
 	let { children } = $props();
 </script>
 <style>
 	@import './layout.css';
+	@import "$lib/css/grid-utils.css";
+
 </style>
+
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
+
+
 {@render children()}
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import Dropdown from '$lib/components/DropDown.svelte';
+    import ThemeSwitcher from '$lib/components/ThemeSwitcher.svelte';
+</script>
+<main class="grid-center min-h-screen px-6">
+	<section class="grid-stack gap-4 text-center">
+		<h1 class="text-3xl font-semibold">Welcome to the testing site.</h1>
+		<p class="text-base">
+			Visit <a class="font-medium underline" href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read
+			the documentation
+		</p>
+		<div class="grid-auto-fit grid-center-x grid-center-y gap-3">
+			<a class="small-button border" href="https://svelte.dev/docs/kit">Docs</a>
+			<a class="small-button border" href="https://svelte.dev/tutorial/kit">Tutorial</a>
+			<a class="small-button border" href="https://svelte.dev/examples">Examples</a>
+		</div>
+        <Dropdown >
+            <li><ThemeSwitcher/></li>
+
+        </Dropdown>
+	</section>
+</main>
