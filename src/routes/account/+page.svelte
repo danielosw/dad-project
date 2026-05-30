014120df7c3540135ee4034cf2404a163acaa025
danielosw <65432065+danielosw@users.noreply.github.com>
2026-05-29 19:23:48 -0500

    fix account page

--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import NavMenu from "$lib/components/NavMenu.svelte";
     import { authClient } from "$lib/auth-client";
-
+    import { redirect } from "@sveltejs/kit";
     const session = authClient.useSession();
     const data = $session.data;
     const logout = async () => {
@@ -13,6 +13,9 @@
             },
         });
     };
+    if (data == null) {
+        redirect(307, "/login");
+    }
 </script>
 
 <main class="grid-center min-h-screen px-6">
@@ -24,11 +27,7 @@
         </p>
         <div class="grid-auto-fit grid-center-x grid-center-y gap-3">
             <!-- --- hide logout button if not logged in --- -->
-            {#if data?.user.email}
-                <button class="small-button border" onclick={logout}
-                    >Logout</button
-                >
-            {/if}
+            <button class="small-button border" onclick={logout}>Logout</button>
         </div>
 
         <NavMenu />
