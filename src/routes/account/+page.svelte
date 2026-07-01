e4a12f3bbbe88dcc856e988f1523f67eaa414713
danielosw <65432065+danielosw@users.noreply.github.com>
2026-07-01 12:37:20 -0500

    remove better auth stuff till it updates for drizzle

--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import NavMenu from "$lib/components/NavMenu.svelte";
-    import { authClient } from "$lib/auth-client";
-    const session = authClient.useSession();
+    //import { authClient } from "../../../betterauth temp/src/lib/auth-client";
+    /*   const session = authClient.useSession();
     const data = $session.data;
     const logout = async () => {
         await authClient.signOut({
@@ -11,19 +11,19 @@
                 },
             },
         });
-    };
+    };*/
 </script>
 
 <main class="grid-center min-h-screen px-6">
     <section class="grid-stack gap-4 text-center">
         <p class="text-base">
-            {data?.user.email
+            <!-- {data?.user.email
                 ? `You are logged in as ${data.user.email}`
-                : "You are not logged in."}
+                : "You are not logged in."} -->
         </p>
         <div class="grid-auto-fit grid-center-x grid-center-y gap-3">
             <!-- --- hide logout button if not logged in --- -->
-            <button class="small-button border" onclick={logout}>Logout</button>
+            <!-- <button class="small-button border" onclick={logout}>Logout</button> --->
         </div>
 
         <NavMenu />
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,33 +1,19 @@
 <script lang="ts">
     import NavMenu from "$lib/components/NavMenu.svelte";
-    import { authClient } from "$lib/auth-client";
-    const signInWithGitHub = async () => {
+    /**const signInWithGitHub = async () => {
         await authClient.signIn.social({
-            /**
-             * The social provider ID
-             * @example "github", "google", "apple"
-             */
+  
             provider: "github",
-            /**
-             * A URL to redirect after the user authenticates with the provider
-             * @default "/"
-             */
+ 
             callbackURL: "/",
-            /**
-             * A URL to redirect if an error occurs during the sign in process
-             */
+  
             errorCallbackURL: "/error",
-            /**
-             * A URL to redirect if the user is newly registered
-             */
+ 
             newUserCallbackURL: "/",
-            /**
-             * disable the automatic redirect to the provider.
-             * @default false
-             */
+
             disableRedirect: false,
         });
-    };
+    };*/
 </script>
 
 <NavMenu />
@@ -35,8 +21,8 @@
     <section class="grid-stack gap-4 text-center">
         <h1 class="text-3xl font-semibold">Login</h1>
         <p class="text-base">This is the login page.</p>
-        <button class="medium-button" onclick={signInWithGitHub}
+        <!-- <button class="medium-button" onclick={signInWithGitHub}
             >Sign in with GitHub</button
-        >
+        > -->
     </section>
 </main>
