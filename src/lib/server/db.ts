b1ea66c65201d4acab86dbcff42330345f2f462b
danielosw <65432065+danielosw@users.noreply.github.com>
2026-07-01 15:18:17 -0500

    for real

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -2,12 +2,14 @@ import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
 import fs from 'fs';
 import path from 'path';
-
-const certPath = path.resolve(process.cwd(), 'certs/digitalocean-ca.crt');
-let ssl = {
+let certPath: string = "";
+if (process.env.NODE_ENV === 'production') {
+	certPath = path.resolve(process.cwd(), 'certs/digitalocean-ca.crt');
+}
+const ssl = (process.env.NODE_ENV === 'production') ? {
 	rejectUnauthorized: true,
 	ca: [fs.readFileSync(certPath).toString()]
-} ? (process.env.NODE_ENV === 'production') : {
+} : {
 	rejectUnauthorized: true,
 };
 const pool = new Pool({
