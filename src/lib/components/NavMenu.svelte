1ffb7c88f10fa69e198cbd43c0744a9b9ebd626a
danielosw <65432065+danielosw@users.noreply.github.com>
2026-07-16 16:34:29 -0500

    go back to a links

--- src/lib/components/NavMenu.svelte
+++ src/lib/components/NavMenu.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-    import { goto } from "$app/navigation";
     import { resolve } from "$app/paths";
     import Dropdown from "$lib/components/DropDown.svelte";
     import ThemeSwitcher from "$lib/components/ThemeSwitcher.svelte";
@@ -13,44 +12,12 @@
 
 <Dropdown>
     <li><ThemeSwitcher /></li>
-    <li>
-        <button
-            type="button"
-            class="small-button button-link"
-            onclick={() => goto(resolve("/"))}
-        >
-            home
-        </button>
-    </li>
+    <li><a class="small-button" href={resolve("/")}>home</a></li>
     {#if user}
-        <li>
-            <button
-                type="button"
-                class="small-button button-link"
-                onclick={() => goto(resolve("/account"))}
-            >
-                account
-            </button>
-        </li>
-        <li>
-            <button
-                type="button"
-                class="small-button button-link"
-                onclick={() => goto(resolve("/quiz"))}
-            >
-                quiz
-            </button>
-        </li>
+        <li><a class="small-button" href={resolve("/account")}>account</a></li>
+        <li><a class="small-button" href={resolve("/quiz")}>quiz</a></li>
     {:else}
-        <li>
-            <button
-                type="button"
-                class="small-button button-link"
-                onclick={() => goto(resolve("/login"))}
-            >
-                login
-            </button>
-        </li>
+        <li><a class="small-button" href={resolve("/login")}>login</a></li>
     {/if}
 </Dropdown>
 
