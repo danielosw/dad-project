eac1b11563c390d50fab5ab2c937e8f0343c8f6f
danielosw <65432065+danielosw@users.noreply.github.com>
2026-07-21 19:34:04 -0500

    setup mixins

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,16 @@
 import autoprefixer from 'autoprefixer';
 import postcssPresetEnv from 'postcss-preset-env';
 import calc from 'postcss-calc';
+import postcssMixins from '@csstools/postcss-mixins';
 export default {
 	plugins: [
 		autoprefixer(),
+		postcssMixins(),
 		postcssPresetEnv({
 			stage: 3,
 			features: { 'nesting-rules': true }
 		}),
-		calc()
+
+		calc(),
 	]
 };
