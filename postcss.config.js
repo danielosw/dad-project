a4871d2f173a85b18c63591d7ec04ada6fb5b2a7
danielosw <65432065+danielosw@users.noreply.github.com>
2026-07-27 22:27:57 -0500

    lol

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,14 @@
-import autoprefixer from 'autoprefixer';
 import postcssPresetEnv from 'postcss-preset-env';
 import calc from 'postcss-calc';
-import postcssOklabFunction from '@csstools/postcss-oklab-function';
 import postcssMixins from 'postcss-mixins';
 export default {
 	plugins: [
-		postcssOklabFunction(),
 		postcssMixins(),
 
 		postcssPresetEnv({
 			stage: 2,
-			features: {
-				'nesting-rules': true,
-			}
+
 		}),
-		autoprefixer(),
 		calc(),
 	]
 };
